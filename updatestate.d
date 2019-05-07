581f1bf005646fe34a81d385bf6293b28a318d28
rohan918 <rohan.narayanan@utexas.edu>
2019-05-06 22:16:36 -0500

    Create updatestate.d

---
+++ updatestate.d
@@ -0,0 +1,6 @@
+.\updatestate.o: UpdateState.c
+.\updatestate.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\updatestate.o: ../inc/tm4c123gh6pm.h
+.\updatestate.o: ST7735.h
+.\updatestate.o: ADC.h
+.\updatestate.o: Shoot.h
