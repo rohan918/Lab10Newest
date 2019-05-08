2889e8d7a1a7287cc4171a84c02b43247c9f4b18
rohan918 <rohan.narayanan@utexas.edu>
2019-05-08 12:32:22 -0500

    Update DAC.c

--- DAC.c
+++ DAC.c
@@ -1,3 +1,46 @@
-// put implementations for functions, explain how it works
-// put your names here, date
+// dac.c
+// This software configures DAC output
+// Lab 6 requires a minimum of 4 bits for the DAC, but you could have 5 or 6 bits
+// Runs on LM4F120 or TM4C123
+// Program written by: put your names here
+// Date Created: 3/6/17 
+// Last Modified: 3/5/18 
+// Lab number: 6
+// Hardware connections
+// TO STUDENTS "REMOVE THIS LINE AND SPECIFY YOUR HARDWARE********
+
+#include <stdint.h>
 #include "../inc/tm4c123gh6pm.h"
+// Code files contain the actual implemenation for public functions
+// this file also contains an private functions and private data
+
+// **************DAC_Init*********************
+// Initialize 4-bit DAC, called once 
+// Input: none
+// Output: none
+void DAC_Init(void){
+	
+	// Intializes ports PB0-PB5 for DAC
+	 SYSCTL_RCGCGPIO_R |= 0x02;
+	//Wait for clocks to turn on
+	 volatile int dummy;
+		dummy ++;
+	
+	GPIO_PORTB_DIR_R |= 0x3F;
+	GPIO_PORTB_DEN_R |= 0x3F;
+	
+	
+	
+	
+}
+
+// **************DAC_Out*********************
+// output to DAC
+// Input: 4-bit data, 0 to 15 
+// Input=n is converted to n*3.3V/15
+// Output: none
+void DAC_Out(uint32_t data){
+	// set pins PB0-PB5 to value between 0-63 which then gets converted to analog by hardware
+	GPIO_PORTB_DATA_R = (data &= 0x3F);
+	
+}
