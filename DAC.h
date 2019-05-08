c591aaceac776bb72e4f35296c92b04bcae56aa5
rohan918 <rohan.narayanan@utexas.edu>
2019-05-08 12:32:25 -0500

    Update DAC.h

--- DAC.h
+++ DAC.h
@@ -1,2 +1,12 @@
+
+
 // put prototypes for public functions, explain what it does
 // put your names here, date
+#ifndef __DAC__
+#define __DAC__
+#include <stdint.h>
+
+void DAC_Init(void);
+void DAC_Out(uint32_t data);
+
+#endif
